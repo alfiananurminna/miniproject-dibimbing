6a5f1937bac4f3f66ce757a5c60be970e8724b07
Alfiana Nur Minna <finakuliah@gmail.com>
2025-02-27 19:33:45 +0700

    additional

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+// Toggle
+const navbarMenu = document.querySelector('.navbar-menu'); 
+const hamburgerMenu = document.querySelector('#hamburger-menu');
+
+hamburgerMenu.onclick = () => {
+    navbarMenu.classList.toggle('active');
+};
+
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+    if(!hamburger.contains(e.target) && !navbarMenu.contains(e.target)) {
+        navbarMenu.classList.remove('active');
+    }
+})
+
+// Submit
 function kirim() {
     alert("Thank You For Registering!");
     return false;
